ff960f62d7b490e3333518a910e7d0cbde097521
Nathsycode <nathsycode@gmail.com>
2023-11-27 15:50:46 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,21 @@ function App() {
     <>
       <div>
         <img className="nc_1" src="https://i.imgur.com/JFDBO4u.jpg" alt="our picture at Tagaytay" />
+        <h3>11-27-2023</h3>
         <p className="initial-message">
-          There is nothing here for now. 💔
+          TIL that it was Jas and co who gave you advices that you should push through with the breakup. Panindigan mo daw. She also said I should stop because I don’t deserve another chance since “ubos” ka na nga. Now I understand when you said you want to stop receiving advices from younger people who knows nothing or understand what we’re going through.
+
+          I thought I was getting better again. I had some things that I was looking forward to: 
+          
+          - Wanted to try coffee making. Aa asked what I want for Christmas and I said coffee grinder. Then I’ll buy myself some aeropress and coffee bean. I was really excited for this.
+          - Also buy an electric guitar and take lessons. Wanted to improve my confidence.
+          - Wanted to also buy a simple bike. Just thought it would be fun.
+          
+          Now I just I want to die, I swear. I am angry at everyone. I am angry at you, your friends, and especially myself. We didn’t need to go through this mess. Fuck everyone. Fuck this life.
+          
+          I miss having someone to help me process my feelings. I understand that everyone just wants the best for us. But nobody quite understands me like you do. I miss having someone to talk to about whatever useless things. I miss holding your hand to give me reassurance that we’ll be fine. I miss your embrace that gives me warmth and hope. I miss your love. Everything.
+          
+          But I don’t deserve you. Especially not at this state. I really want to improve on myself first, at least.
         </p>
       </div>
     </>
